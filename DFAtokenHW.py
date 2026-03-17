ba502020a7e1e23b226527b3d12262a795fa2b22
Camila Quintanilla <camilaquintanillacabrera@gmail.com>
2026-03-16 19:59:54 -0600

    cleaning text file

--- DFAtokenHW.py
+++ DFAtokenHW.py
@@ -1,3 +1,7 @@
 def lexerAritmetico(archivo):
     with open (archivo) as f:
-        for line in f: lol
+        for line in f: 
+            line = line.strip()
+
+            if line == "":
+                continue
